9c26d080cff585823d7720dcb80555e3a7af34e3
Robin Buschmann <cm@Robins-MacBook-Pro.local>
2016-03-22 08:56:39 +0100

    export object flag and functionality implemented

--- index.js
+++ index.js
@@ -5,37 +5,43 @@ var yargs = require('yargs');
 
 // cli configuration
 var argv = yargs
-    .usage('ngconfig [configJsonPath] [outputJsPath] [--moduleName|-m <string>] [--useExistingModule|-x <boolean>] [--imports|-i <array>] [--exports|-e <string>]')
-    .example('ngconfig src/config.dev.json src/app/config.js -m configuration')
-    .example('ngconfig src/config.prod.json src/app/config.js -m configuration -x')
-    .example('ngconfig src/config.prod.json src/app/config.ts -m configuration -e config')
-    .example('ngconfig src/config.prod.json src/app/config.ts -m configuration -e config -i path/to/dependency another/path')
-    .option('m', {
-        alias: 'moduleName',
-        type: 'string',
-        describe: 'Name of angular module',
-        default: 'config'
-    })
-    .option('x', {
-        alias: 'useExistingModule',
-        type: 'boolean',
-        describe: 'Defines whether a new module should be created or not',
-        default: false
-    })
-    .option('i', {
-        alias: 'imports',
-        type: 'array',
-        describe: 'Paths for es6 modules',
-        default: []
-    })
-    .option('e', {
-        alias: 'export',
-        type: 'string',
-        describe: 'Export name for es6 modules',
-        default: false
-    })
-    .demand(2)
-    .argv;
+  .usage('ngconfig [configJsonPath] [outputJsPath] [--moduleName|-m <string>] [--useExistingModule|-x <boolean>] [--imports|-i <array>] [--exports|-e <string>]')
+  .example('ngconfig src/config.dev.json src/app/config.js -m configuration')
+  .example('ngconfig src/config.prod.json src/app/config.js -m configuration -x')
+  .example('ngconfig src/config.prod.json src/app/config.ts -m configuration -e config')
+  .example('ngconfig src/config.prod.json src/app/config.ts -m configuration -e config -i path/to/dependency another/path -o')
+  .option('m', {
+    alias: 'moduleName',
+    type: 'string',
+    describe: 'Name of angular module',
+    default: 'config'
+  })
+  .option('x', {
+    alias: 'useExistingModule',
+    type: 'boolean',
+    describe: 'Defines whether a new module should be created or not',
+    default: false
+  })
+  .option('i', {
+    alias: 'imports',
+    type: 'array',
+    describe: 'Paths for es6 modules',
+    default: []
+  })
+  .option('e', {
+    alias: 'export',
+    type: 'string',
+    describe: 'Export name for es6 modules',
+    default: false
+  })
+  .option('o', {
+    alias: 'exportObjects',
+    type: 'boolean',
+    describe: 'Export constants directly',
+    default: false
+  })
+  .demand(2)
+  .argv;
 
 // main arguments
 var configPath = argv._[0];
@@ -43,12 +49,13 @@ var ngConfigOutPath = argv._[1];
 
 // ngConfig options
 var options = {
-    template: path.join(__dirname, './module.tpl'),
-    module: argv.moduleName,
-    useExistingModule: argv.useExistingModule,
-    _export: argv.export,
-    imports: argv.imports,
-    constants: JSON.parse(fs.readFileSync(configPath))
+  template: path.join(__dirname, './module.tpl'),
+  module: argv.moduleName,
+  useExistingModule: argv.useExistingModule,
+  _export: argv.export,
+  exportObjects: argv.exportObjects,
+  imports: argv.imports,
+  constants: JSON.parse(fs.readFileSync(configPath))
 };
 
 // create angular constants template string
--- module.tpl
+++ module.tpl
@@ -1,6 +1,16 @@
 <% imports.forEach(function(value) { %>
 import '<%= value %>';
 <% }); %>
-<% if(_export) { %> export var <%= _export %> = <% } %>
-angular.module('<%= module %>'<% if(!useExistingModule) { %>, []<% } %>)<% Object.keys(constants).forEach(function(key) { %>
-  .constant('<%= key %>', <%= JSON.stringify(constants[key]) %>)<% }); %>;
+
+<% if(exportObjects) Object.keys(constants).forEach(function(key) { %>
+export const <%= key %> = <%= JSON.stringify(constants[key]) %>;
+<% }); %>
+
+<% if(_export) { %> export const <%= _export %> = <% } %>
+angular.module('<%= module %>'<% if(!useExistingModule) { %>, []<% } %>)
+
+<% Object.keys(constants).forEach(function(key) { %>
+  .constant('<%= key %>', <% if(exportObjects) %><%= key %><% else %><%= JSON.stringify(constants[key]) %><% ; %>)
+  <% }); %>;
+
+
